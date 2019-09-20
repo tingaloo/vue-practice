5eb1ae64a9ff301c35b3dc80788b21fdc1f95f2e
Ting Lew <ting.lew@gmail.com>
2019-09-19 22:53:12 -0400

    create base project and google search homepage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <GoogleLayout/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import GoogleLayout from './components/GoogleLayout.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GoogleLayout
   }
 }
 </script>
---
+++ src/components/GoogleLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex self-center justify-center h-screen flex-col justify-between">
+    <div class="flex justify-between pt-4 px-2">
+      <div>
+        <a class="px-2 tracking-tight">About</a>
+        <a class="px-2 tracking-tight">Store</a>
+      </div>
+      <div>
+        <a class="px-2">Gmail</a>
+        <a class="px-2">Images</a>
+        <a class="px-2">Icon</a>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <div class="flex justify-center py-4">
+        <img class="object-contain w-1/4" src="../assets/googleLogo.png" />
+      </div>
+      <div class="flex justify-center">
+        <div class="flex justify-center border rounded-sm border-gray-200 py-2 w-3/5 px-2">
+          <div class>
+            <img class="inline-block align-middle h-4" src="../assets/searchIcon.png" />
+          </div>
+          <input class="flex-grow mx-2" v-model="message" placeholder="edit me" />
+          <div class>
+            <img class="inline-block align-middle h-4" src="../assets/mic.png" />
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center pt-8">
+        <div
+          class="px-2 mx-2 py-2 bg-gray-100 cursor-pointer border border-white hover:border-gray-500"
+        >Google Search</div>
+        <div
+          class="px-2 mx-2 py-2 bg-gray-100 cursor-pointer border border-white hover:border-gray-500"
+        >I'm Feeling Lucky</div>
+      </div>
+    </div>
+    <div class="flex justify-between bg-gray-100 py-4 px-4">
+      <div>
+        <a class="px-2 tracking-tight text-xs">Advertising</a>
+        <a class="px-2 tracking-tight text-xs">Business</a>
+        <a class="px-2 tracking-tight text-xs">How Search works</a>
+      </div>
+      <div>
+        <a class="px-4 text-xs">Privacy</a>
+        <a class="px-2 text-xs">Terms</a>
+        <a class="px-2 text-xs">Settings</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoogleLayout",
+  props: {}
+};
+</script>
